6a5131f9b056293f2c4400026ea050a122766de5
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-08 16:10:13 +0200

    ajout du chat fonctionnel sans style

---
+++ script.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+//var fs=require ('fs');
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'))
+app.get('/',function(req,res){
+	res.render('superchat')
+});
+
+server=app.listen(2727);
+
+var io=require('socket.io')(server);
+
+io.on('connection',function(socket){
+	socket.username =""
+	socket.on('username',function(data){
+		socket.username=data.username
+	});
+	socket.on('msg',function(data){
+		io.sockets.emit('msg',{message: data.message,username : socket.username})
+	})
+});
+
